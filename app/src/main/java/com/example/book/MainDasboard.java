da87aea713a855138ab3554013f1332fc38e88e0
Tyasa Dewa Pamungkas <tyasadewa@gmail.com>
2019-08-12 09:01:52 +0700

    update fragment

--- app/src/main/java/com/example/book/MainDasboard.java
+++ app/src/main/java/com/example/book/MainDasboard.java
@@ -1,46 +1,62 @@
 package com.example.book;
 
 import android.app.Activity;
+import android.app.FragmentManager;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 
-public class MainDasboard extends Activity {
-    private TextView mTextMessage;
+public class MainDasboard extends AppCompatActivity
+        implements BottomNavigationView.OnNavigationItemSelectedListener {
 
-    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener
-            = new BottomNavigationView.OnNavigationItemSelectedListener() {
-
-        @Override
-        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-            switch (item.getItemId()) {
-                case R.id.navigation_request:
-                    mTextMessage.setText(R.string.title_request);
-                    return true;
-                case R.id.navigation_listbooking:
-                    mTextMessage.setText(R.string.title_listbooking);
-                    return true;
-                case R.id.navigation_approval:
-                    mTextMessage.setText(R.string.title_listapproval);
-                    return true;
-                case  R.id.navigation_logout:
-                    mTextMessage.setText((R.string.title_logout));
-                    return true;
-            }
-            return false;
-        }
-    };
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_dasboard);
-        BottomNavigationView navView = findViewById(R.id.nav_view);
-        mTextMessage = findViewById(R.id.message);
-        navView.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
+
+        // kita set default nya Request Fragment
+        loadFragment(new RequestFragment());
+        // inisialisasi BottomNavigaionView
+        BottomNavigationView bottomNavigationView = findViewById(R.id.nav_view);
+        // beri listener pada saat item/menu bottomnavigation terpilih
+        bottomNavigationView.setOnNavigationItemSelectedListener(this);
+    }
+
+    private boolean loadFragment(Fragment fragment){
+        if (fragment != null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()){
+            case R.id.navigation_request:
+                fragment = new RequestFragment();
+                break;
+            case R.id.navigation_listbooking:
+                fragment = new ListbookingFragment();
+                break;
+            case R.id.navigation_approval:
+                fragment = new ApprovalFragment();
+                break;
+            case R.id.navigation_logout:
+                fragment = new LogoutFragment();
+                break;
+        }
+        return loadFragment(fragment);
     }
 }
